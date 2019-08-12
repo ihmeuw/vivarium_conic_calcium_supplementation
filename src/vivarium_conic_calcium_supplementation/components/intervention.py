7fc10a4968e37589e60de6bc68ab7f80d9c4ebad
Cody <chorst@uw.edu>
2019-08-12 16:16:11 -0700

    wip: pop and individual random effects

--- src/vivarium_conic_calcium_supplementation/components/intervention.py
+++ src/vivarium_conic_calcium_supplementation/components/intervention.py
@@ -1,13 +1,31 @@
 import pandas as pd
+import numpy as np
+import scipy.stats
 
 
 class CalciumSupplementationIntervention:
 
     configuration_defaults = {
         'calcium_supplementation_intervention': {
-            'proportion': 1.0,
-            'birth_weight_shift': 0,  # grams
-            'gestation_time_shift': 0,  # weeks
+            'proportion': 0.5,
+            'birth_weight_shift': {   # grams
+                'population': {
+                    'mean': 0.5,
+                    'sd': 0.25
+                },
+                'individual': {
+                    'sd': 0.25
+                }
+            },
+            'gestation_time_shift': {  # weeks
+                'population': {
+                    'mean': 0.5,
+                    'sd': 0.25
+                },
+                'individual': {
+                    'sd': 0.25
+                }
+            },
             'stunting_shift': 0,  # z-score
             'wasting_shift': 0,  # z-score
             'underweight_shift': 0,  # z-score
@@ -20,12 +38,18 @@ class CalciumSupplementationIntervention:
     def setup(self, builder):
         self.start_time = pd.Timestamp(**builder.configuration.time.start.to_dict())
         self.config = builder.configuration['calcium_supplementation_intervention']
+
         validate_configuration(self.config.to_dict())
-        self.randomness = builder.randomness.get_stream('calcium_supplementation_intervention_enrollment')
+
+        self.enrollment_randomness = builder.randomness.get_stream('calcium_supplementation_intervention_enrollment')
+        self.effect_randomness = builder.randomness.get_stream('effect_draw')
+
         columns_created = ['calcium_supplementation_treatment_status']
         self.population_view = builder.population.get_view(columns_created)
+
         builder.population.initializes_simulants(self.on_initialize_simulants,
                                                  creates_columns=columns_created)
+
         builder.value.register_value_modifier('low_birth_weight_and_short_gestation.raw_exposure',
                                               self.adjust_lbwsg)
         builder.value.register_value_modifier('child_stunting.exposure',
@@ -35,14 +59,39 @@ class CalciumSupplementationIntervention:
         builder.value.register_value_modifier('child_underweight.exposure',
                                               self.adjust_underweight)
 
+        self.pop_birth_weight_mean = self.get_effect_size(self.config.birth_weight_shift.population.mean,
+                                                          self.config.birth_weight_shift.population.sd,
+                                                          'population_birth_weight')
+        self.pop_gestation_time_mean = self.get_effect_size(self.config.gestation_time_shift.population.mean,
+                                                            self.config.gestation_time_shift.population.sd,
+                                                            'population_gestation_time')
+        self.ind_birth_weight_effect = pd.Series()
+        self.ind_gestation_time_effect = pd.Series()
+
     def on_initialize_simulants(self, pop_data):
         pop = pd.DataFrame({'calcium_supplementation_treatment_status': 'not_treated'}, index=pop_data.index)
         if pop_data.creation_time > self.start_time:
             treatment_probability = self.config.proportion
-            treated = self.randomness.filter_for_probability(pop.index, treatment_probability)
+            treated = self.enrollment_randomness.filter_for_probability(pop.index, treatment_probability)
             pop.loc[treated, 'calcium_supplementation_treatment_status'] = 'treated'
 
+            ind_birth_effect = self.get_effect_size(self.pop_birth_weight_mean,
+                                                    self.config.birth_weight_shift.individual.sd,
+                                                    'individual_birth_weight')
+            self.ind_birth_weight_effect = self.ind_birth_weight_effect.append(ind_birth_effect, index=pop_data.index)
+
+            ind_gestation_effect = self.get_effect_size(self.pop_gestation_time_mean,
+                                                        self.config.gestation_time_shift.individual.sd,
+                                                        'individual_gestation_time')
+            self.ind_gestation_time_effect = self.ind_gestation_time_effect.append(ind_gestation_effect, index=pop_data.index)
+
         self.population_view.update(pop)
+
+    def get_effect_size(self, mean, sd, key):
+        r = np.random.RandomState(self.effect_randomness.get_seed(additional_key=key))
+        draw = r.uniform()
+        effect = scipy.stats.norm(mean, sd).ppf(draw)
+        return effect
 
     def adjust_lbwsg(self, index, exposure):
         pop = self.population_view.get(index)
@@ -68,5 +117,8 @@ def validate_configuration(config):
         raise ValueError(f'The proportion for calcium supplementation intervention must be between 0 and 1.'
                          f'You specified {config.proportion}.')
     for key in config:
-        if 'shift' in key and config[key] < 0:
+        if 'shift' in key and isinstance(config[key], int) and config[key] < 0:
             raise ValueError(f'Additive shift for {key} must be positive.')
+
+    # TODO: Test the nested dictionaries
+
