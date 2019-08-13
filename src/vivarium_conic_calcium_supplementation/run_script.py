6de44aa83d473fde353110f60c7f293c3626fe5c
Cody Horst <chorst@uw.edu>
2019-08-13 15:51:50 -0700

    remove run script

--- src/vivarium_conic_calcium_supplementation/run_script.py
+++
@@ -1,4 +0,0 @@
-from vivarium.interface import setup_simulation_from_model_specification
-
-sim = setup_simulation_from_model_specification('./model_specifications/vivarium_conic_calcium_supplementation_mali.yaml')
-sim.run()
