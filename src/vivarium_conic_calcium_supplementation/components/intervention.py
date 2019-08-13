f4e8a08c23bd8625a56257cec62094cb38125836
Cody <chorst@uw.edu>
2019-08-12 17:26:39 -0700

    incorporate individual effects into lbwsg adjustment

--- src/vivarium_conic_calcium_supplementation/components/intervention.py
+++ src/vivarium_conic_calcium_supplementation/components/intervention.py
@@ -102,8 +102,8 @@ class CalciumSupplementationIntervention:
 
     def adjust_lbwsg(self, index, exposure):
         pop = self.population_view.get(index)
-        exposure['birth_weight'] += self.config.birth_weight_shift * (pop.calcium_supplementation_treatment_status == 'treated')
-        exposure['gestation_time'] += self.config.gestation_time_shift * (pop.calcium_supplementation_treatment_status == 'treated')
+        exposure['birth_weight'] += self.ind_birth_weight_effect * (pop.calcium_supplementation_treatment_status == 'treated')
+        exposure['gestation_time'] += self.ind_gestation_time_effect * (pop.calcium_supplementation_treatment_status == 'treated')
         return exposure
 
     def adjust_stunting(self, index, exposure):
