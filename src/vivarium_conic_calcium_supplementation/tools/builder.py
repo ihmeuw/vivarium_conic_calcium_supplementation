4cc604175d4cb25594ff5a9d0c5f4c57ad41cb2e
Cody <chorst@uw.edu>
2020-01-08 11:33:45 -0800

    refactor all getters through one safe write function

--- src/vivarium_conic_calcium_supplementation/tools/builder.py
+++ src/vivarium_conic_calcium_supplementation/tools/builder.py
@@ -1,6 +1,6 @@
 import itertools
 from pathlib import Path
-from typing import Sequence
+from typing import Sequence, Mapping
 
 from loguru import logger
 
@@ -8,26 +8,23 @@ from vivarium.framework.artifact import EntityKey, get_location_term, Artifact
 from vivarium_inputs.data_artifact.loaders import loader
 
 
-def create_new_artifact(path: str, location: str) -> Artifact:
-    logger.info(f"Creating artifact at {path}.")
-    artifact = Artifact(path, filter_terms=[get_location_term(location)])
-    key = EntityKey('metadata.locations')
-    if str(key) not in artifact:
-        logger.info(f'writing {key}.')
-        artifact.write('metadata.locations', [location])
-    else:
-        logger.info(f'{key} found in artifact.')
-    return artifact
-
-
-def safe_write(artifact: Artifact, keys: Sequence, location: str):
+def safe_write(artifact: Artifact, keys: Sequence, getters: Mapping):
     for key in keys:
         if str(key) not in artifact:
-            logger.info(f'writing {key}.')
-            data = loader(key, location, set())
+            logger.info(f'>>> writing {key}.')
+            data = getters[key]()
             artifact.write(key, data)
         else:
             logger.info(f'{key} found in artifact.')
+
+
+def create_new_artifact(path: str, location: str) -> Artifact:
+    logger.info(f"Creating artifact at {path}.")
+
+    artifact = Artifact(path, filter_terms=[get_location_term(location)])
+    key = EntityKey('metadata.locations')
+    safe_write(artifact, [key], {key: lambda: [location]})
+    return artifact
 
 
 def write_demographic_data(artifact: Artifact, location: str):
@@ -37,7 +34,8 @@ def write_demographic_data(artifact: Artifact, location: str):
             EntityKey('population.age_bins'),
             EntityKey('population.theoretical_minimum_risk_life_expectancy'),
             EntityKey('population.demographic_dimensions')]
-    safe_write(artifact, keys, location)
+    getters = {k: lambda: loader(k, location, set()) for k in keys}
+    safe_write(artifact, keys, getters)
 
 
 def write_covariate_data(artifact: Artifact, location: str):
@@ -47,7 +45,8 @@ def write_covariate_data(artifact: Artifact, location: str):
     measures = ['estimate']
 
     keys = [EntityKey(f'covariate.{c}.{m}') for c, m in itertools.product(covariates, measures)]
-    safe_write(artifact, keys, location)
+    getters = {k: lambda: loader(k, location, set()) for k in keys}
+    safe_write(artifact, keys, getters)
 
 
 def write_disease_data(artifact: Artifact, location: str):
@@ -77,26 +76,27 @@ def write_disease_data(artifact: Artifact, location: str):
             ['cause_specific_mortality_rate', 'excess_mortality_rate', 'disability_weight',
              'restrictions'],
     }
-
     for cause, measures in cause_measures.items():
         keys = [EntityKey(f'cause.{cause}.{m}') for m in measures]
-        safe_write(artifact, keys, location)
+        getters = {k: lambda: loader(k, location, set()) for k in keys}
+        safe_write(artifact, keys, getters)
 
 
 def write_alternative_risk_data(artifact, location):
     logger.info('Writing risk data...')
 
     risks = ['child_wasting', 'child_underweight', 'child_stunting']
+    measures = ['relative_risk', 'population_attributable_fraction']
     alternative_measures = ['exposure', 'exposure_distribution_weights', 'exposure_standard_deviation']
     keys = [EntityKey(f'alternative_risk_factor.{r}.{m}') for r, m in itertools.product(risks, alternative_measures)]
-    safe_write(artifact, keys, location)
-
-    measures = ['relative_risk', 'population_attributable_fraction']
-    keys = [EntityKey(f'risk_factor.{r}.{m}') for r, m in itertools.product(risks, measures)]
-    safe_write(artifact, keys, location)
+    keys.extend([EntityKey(f'risk_factor.{r}.{m}') for r, m in itertools.product(risks, measures)])
+    getters = {k: lambda: loader(k, location, set()) for k in keys}
+    safe_write(artifact, keys, getters)
 
 
 def write_lbwsg_data(artifact, location):
+    logger.info('Writing low birth weight and short gestation data...')
+
     risk = 'low_birth_weight_and_short_gestation'
     measures = ['exposure', 'population_attributable_fraction', 'relative_risk']
     keys = [EntityKey(f'risk_factor.{risk}.{m}') for m in measures]
@@ -105,15 +105,10 @@ def write_lbwsg_data(artifact, location):
     if location in ['Mali']:
         data_source = Path('/share/costeffectiveness/lbwsg/artifacts') / f"{location.replace(' ', '_')}.hdf"
         reversioned_artifact = Artifact(data_source)
-        for key in keys:
-            if str(key) not in artifact:
-                logger.info(f'writing {key}.')
-                data = reversioned_artifact.load(str(key))
-                artifact.write(key, data)
-            else:
-                logger.info(f'{key} found in artifact.')
+        getters = {k: lambda: reversioned_artifact.load(str(k)) for k in keys}
     else:
-        safe_write(artifact, keys, location)
+        getters = {k: lambda: loader(k, location, set()) for k in keys}
+    safe_write(artifact, keys, getters)
 
 
 def build_artifact(location: str, output_dir: str, erase: bool):
